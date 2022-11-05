4577d1e7ca5b21f2046f9029a7ee864b9270163c
Fermin Yanes <ferminyanes07@gmail.com>
2022-11-04 21:15:21 -0500

    first commit

---
+++ Deliverable2 SQL.sql
@@ -0,0 +1,57 @@
+CREATE TABLE "User" (
+  "Id" SERIAL PRIMARY KEY NOT NULL,
+  "Name" varchar NOT NULL,
+  "Lastname" varchar NOT NULL,
+  "Password" varchar NOT NULL,
+  "Email" varchar NOT NULL,
+  "Age" integer,
+  "Status" varchar NOT NULL
+);
+
+CREATE TABLE "Post" (
+  "Id" SERIAL PRIMARY KEY NOT NULL,
+  "Title" varchar,
+  "Description" varchar,
+  "User_Id" integer UNIQUE NOT NULL,
+  "Content" varchar,
+  "Status" varchar
+);
+
+CREATE TABLE "Category" (
+  "Id" SERIAL PRIMARY KEY NOT NULL,
+  "rel_Category_Post_Id" integer,
+  "Name" varchar NOT NULL,
+  "Status" varchar
+);
+
+CREATE TABLE "Comment" (
+  "Id" SERIAL PRIMARY KEY NOT NULL,
+  "User_Id" integer,
+  "rel_Comment_Post" integer,
+  "Content" varchar NOT NULL,
+  "Status" varchar
+);
+
+CREATE TABLE "rel_Category_Post" (
+  "Id" SERIAL PRIMARY KEY,
+  "Post_Id" integer,
+  "Category_Id" integer
+);
+
+CREATE TABLE "rel_Comment_Post" (
+  "Id" SERIAL PRIMARY KEY,
+  "Post_Id" integer,
+  "Comment_Id" integer
+);
+
+ALTER TABLE "Comment" ADD FOREIGN KEY ("User_Id") REFERENCES "User" ("Id");
+
+ALTER TABLE "Post" ADD FOREIGN KEY ("User_Id") REFERENCES "User" ("Id");
+
+ALTER TABLE "rel_Comment_Post" ADD FOREIGN KEY ("Post_Id") REFERENCES "Post" ("Id");
+
+ALTER TABLE "rel_Comment_Post" ADD FOREIGN KEY ("Comment_Id") REFERENCES "Comment" ("Id");
+
+ALTER TABLE "rel_Category_Post" ADD FOREIGN KEY ("Post_Id") REFERENCES "Post" ("Id");
+
+ALTER TABLE "rel_Category_Post" ADD FOREIGN KEY ("Category_Id") REFERENCES "Category" ("Id");
